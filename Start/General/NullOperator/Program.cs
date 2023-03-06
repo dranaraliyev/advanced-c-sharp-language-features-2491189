e3784c4a9faef494bb4cb7a8688ec906a0a25628
dranaraliyev <aliyevan@hu-berlin.de>
2023-03-06 18:50:29 -0500

    Language Constructs

--- Start/General/NullOperator/Program.cs
+++ Start/General/NullOperator/Program.cs
@@ -2,22 +2,34 @@
 // The null coalescing operator
 
 // A common scenario is to test a value for null and assign one if it is
-void OldSchoolLogString(string theString) {
+/* void OldSchoolLogString(string? theString) {
     if (theString == null) {
         Console.WriteLine("No string given!");
     }
     else {
         Console.WriteLine(theString);
     }
-}
+} */
 
-OldSchoolLogString("Test String");
-OldSchoolLogString(null);
+/* OldSchoolLogString("Test String");
+OldSchoolLogString(null); */
 
 // TODO: the ?? operator returns the left-hand value if not null, or the right one if it is null
+// void LogString(string? theString){
+//     System.Console.WriteLine(theString ?? "No String Given!");
+// }
 
+// LogString("Test String");
+// LogString(null);
+// // TODO: It's also allowable to throw an exception as part of the right-hand expression
+// void ThrowableLogString(string? theString) {
+//     System.Console.WriteLine(theString ?? 
+//             throw new ArgumentNullException("theString", "cannot be null!"));
+// }
 
-// TODO: It's also allowable to throw an exception as part of the right-hand expression
-
+// ThrowableLogString(null);
 
 // TODO: The ??= assigns a value if the left-hand value is null
+string? str = null;
+str ??= "Default Value";
+System.Console.WriteLine(str);
